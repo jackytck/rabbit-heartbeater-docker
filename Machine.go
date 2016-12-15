4dddc56671c8bc45d723356531867973751d83c1
Jacky Tang <jackytck@gmail.com>
2016-12-15 16:56:50 +0800

    :sparkles: store machine status + check up / down

--- Machine.go
+++ Machine.go
@@ -1,11 +1,67 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
 // Machine represents one of the alti-worker.
 type Machine struct {
-	Name     string   `json:"name"`
-	Nickname string   `json:"nickname"`
-	Type     string   `json:"type"`
-	Ping     JSONTime `json:"ping"`
-	Pong     JSONTime `json:"pong"`
-	Extra    string   `json:"extra"`
+	Name     string          `json:"name"`
+	Nickname string          `json:"nickname"`
+	Type     string          `json:"type"`
+	Ping     JSONTime        `json:"ping"`
+	Pong     JSONTime        `json:"pong"`
+	Response []time.Duration `json:"response"`
+	Extra    string          `json:"extra"`
+	Status   string          `json:"status"`
+}
+
+// Update updates the struct in db.
+func (m *Machine) Update(session *mgo.Session) {
+	if m.Name == "" || m.Nickname == "" || m.Type == "" {
+		return
+	}
+	db := LoadDBName()
+	c := session.DB(db).C("machine")
+
+	// calculate response time
+	ping := time.Time(m.Ping)
+	pong := time.Time(m.Pong)
+	d := pong.Sub(ping)
+
+	// get existing
+	old := Machine{}
+	useHist := true
+	c.Find(bson.M{"name": m.Name}).One(&old)
+	if old.Status == "" || old.Status == "down" {
+		LogGreen(m.Name + " is up!")
+		useHist = false
+	}
+
+	// append response to old
+	if useHist {
+		m.Response = append(m.Response, old.Response...)
+	}
+	m.Response = append(m.Response, d)
+	m.Status = "up"
+
+	_, err := c.Upsert(bson.M{"name": m.Name}, m)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// SetDown sets the status to "down".
+func (m *Machine) SetDown(session *mgo.Session) {
+	db := LoadDBName()
+	c := session.DB(db).C("machine")
+	m.Status = "down"
+	_, err := c.Upsert(bson.M{"name": m.Name}, m)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"log"
 	"time"
 
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
 	"github.com/robfig/cron"
 	"github.com/streadway/amqp"
 )
 
-func listenPong(ch *amqp.Channel, msgs <-chan amqp.Delivery) {
+func listenPong(ch *amqp.Channel, msgs <-chan amqp.Delivery, db *mgo.Session) {
 	for d := range msgs {
 		LogCyan("Received a Pong:")
 		log.Println(string(d.Body))
@@ -17,6 +20,7 @@ func listenPong(ch *amqp.Channel, msgs <-chan amqp.Delivery) {
 		json.Unmarshal(d.Body, &mach)
 		machStr, _ := json.MarshalIndent(mach, "", "  ")
 		LogMagenta("\n" + string(machStr))
+		mach.Update(db)
 		// // set start time
 		// proj.DateStartArchive = JSONTime(time.Now())
 		// err := archive(dataPath, proj.PID)
@@ -51,6 +55,24 @@ func listenPong(ch *amqp.Channel, msgs <-chan amqp.Delivery) {
 	}
 }
 
+// checkTimeout checks if any machine has timeout, and thus set its status to
+// 'down'.
+func checkTimeout(session *mgo.Session) {
+	db := LoadDBName()
+	c := session.DB(db).C("machine")
+	iter := c.Find(bson.M{"status": "up"}).Iter()
+	var machines []Machine
+	iter.All(&machines)
+	for _, m := range machines {
+		pong := time.Time(m.Pong)
+		d := time.Since(pong)
+		if d.Seconds() > 60 {
+			LogRed(m.Name + " is down!")
+			m.SetDown(session)
+		}
+	}
+}
+
 func ping(ch *amqp.Channel, ex string) {
 	LogCyan("Sending Ping...")
 	machine := Machine{}
@@ -62,27 +84,44 @@ func ping(ch *amqp.Channel, ex string) {
 }
 
 func main() {
-	uri := LoadURI()
+	// environment variables
+	rabbitURI := LoadURI("rabbit")
+	mongoURI := LoadURI("mongo")
 	pingName := LoadRabbitName("ping")
 	pongName := LoadRabbitName("pong")
 
-	conn, ch := ConnectRabbit(uri)
+	// connect to mongo
+	dbSession, err := mgo.Dial(mongoURI)
+	if err != nil {
+		panic(err)
+	}
+	defer dbSession.Close()
+
+	// connect to rabbit
+	conn, ch := ConnectRabbit(rabbitURI)
 	defer conn.Close()
 	defer ch.Close()
 
+	// braodcast via exchange per 60 seconds
 	DeclareExchange(ch, pingName)
-	// broadcast to all machines per 60 seconds
 	c := cron.New()
 	c.AddFunc("@every 60s", func() { ping(ch, pingName) })
-	c.Start()
+	ping(ch, pingName)
 
 	// listen to Pong from any responding machine
 	DeclareQueue(ch, pongName)
 	pongMsgs := ConsumeQueue(conn, ch, pongName, 1)
-	go listenPong(ch, pongMsgs)
+	go listenPong(ch, pongMsgs, dbSession)
 
+	// check for any timeout from existing "up" machines
+	c.AddFunc("@every 60s", func() { checkTimeout(dbSession) })
+	checkTimeout(dbSession)
+
+	// start cron
+	c.Start()
+
+	// listen by forever blocking
 	forever := make(chan bool)
 	// LogBlackOnWhite("[*] Waiting for messages. To exit press CTRL+C")
-
 	<-forever
 }
